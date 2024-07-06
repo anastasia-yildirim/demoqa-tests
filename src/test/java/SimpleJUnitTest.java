74051817b75547524817b6ced99ee39ebca5f548
anastasiayildirim <anastasia.skrydlova@gmail.com>
2024-07-06 19:36:23 +0300

    updated SimpleJUnitTest

--- src/test/java/SimpleJUnitTest.java
+++ src/test/java/SimpleJUnitTest.java
@@ -1,10 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class SimpleJUnitTest {
 
     @Test
