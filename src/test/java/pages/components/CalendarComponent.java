a94d5810964e4f18b622bb543041ad615b851436
anastasiayildirim <anastasia.skrydlova@gmail.com>
2024-08-21 22:12:17 +0300

    added check for number of characters in 'day' string for setDate();

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,13 +1,15 @@
 package pages.components;
 
-import com.codeborne.selenide.SelenideElement;
-
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
+        if (day.length() == 1) {
+            day = "0" + day;
+        }
+
         $(".react-datepicker__day.react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)")
                 .click();
     }
